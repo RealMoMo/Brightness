c558df6c05822d74a625dc27db2161c5273fcc52
炜烨 <czb166@qq.com>
2018-09-19 18:03:44 +0800

    update example demo
    update example demo

--- app/src/main/java/com/hht/brightness/lib/test/MainActivity.java
+++ app/src/main/java/com/hht/brightness/lib/test/MainActivity.java
@@ -2,12 +2,67 @@ package com.hht.brightness.lib.test;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
 
-public class MainActivity extends AppCompatActivity {
+import com.hht.brightness.BrightnessPlatform;
+import com.hht.brightness.impl.BaseBrightnessImpl;
+import com.hht.brightness.impl.BrightnessFactory;
+import com.hht.brightness.strategy.change.IBrightnessChange;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private SeekBar sb_standard_imm;
+    private SeekBar sb_standard_gra;
+
+    private Button btn1,btn2;
+
+    private BaseBrightnessImpl standardImm;
+    private BaseBrightnessImpl standardGra;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initBrightness();
+        initView();
+
+    }
+
+    private void initBrightness() {
+        standardImm = BrightnessFactory.createBrightnessImpl(getApplication(), BrightnessPlatform.PLATFORM_STANDARD, IBrightnessChange.Strategy.IMMEDIATELY);
+        standardGra = BrightnessFactory.createBrightnessImpl(getApplication(), BrightnessPlatform.PLATFORM_STANDARD, IBrightnessChange.Strategy.GRADIENT);
+    }
+
+    private void initView() {
+
+        sb_standard_imm = findViewById(R.id.sb_standard_imm);
+        sb_standard_gra  = findViewById(R.id.sb_standard_gra);
+
+        btn1 = findViewById(R.id.btn1);
+        btn2 = findViewById(R.id.btn2);
+        btn1.setOnClickListener(this);
+        btn2.setOnClickListener(this);
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn1:{
+                standardImm.setBrightness(new Random().nextInt(100));
+            }break;
+            case R.id.btn2:{
+                standardGra.setBrightness(new Random().nextInt(100));
+            }break;
+        }
     }
 }

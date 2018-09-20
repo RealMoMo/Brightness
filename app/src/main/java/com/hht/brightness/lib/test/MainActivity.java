7182642835b0d39f05887db87402e39f9583f36a
炜烨 <czb166@qq.com>
2018-09-20 11:18:22 +0800

    update example demo
    update example demo

--- app/src/main/java/com/hht/brightness/lib/test/MainActivity.java
+++ app/src/main/java/com/hht/brightness/lib/test/MainActivity.java
@@ -7,14 +7,14 @@ import android.widget.Button;
 import android.widget.SeekBar;
 
 import com.hht.brightness.BrightnessPlatform;
-import com.hht.brightness.i.StatusListener;
+import com.hht.brightness.i.CustomChangeListener;
 import com.hht.brightness.impl.BaseBrightnessImpl;
 import com.hht.brightness.impl.BrightnessFactory;
 import com.hht.brightness.strategy.change.IBrightnessChange;
 
 import java.util.Random;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, SeekBar.OnSeekBarChangeListener {
 
     private SeekBar sb_standard_imm;
     private SeekBar sb_standard_gra;
@@ -38,106 +38,48 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     }
 
-    private void initBrightness() {
-        standardImm = BrightnessFactory.createBrightnessImpl(getApplication(), BrightnessPlatform.PLATFORM_STANDARD, IBrightnessChange.Strategy.IMMEDIATELY);
-        standardGra = BrightnessFactory.createBrightnessImpl(getApplication(), BrightnessPlatform.PLATFORM_STANDARD, IBrightnessChange.Strategy.GRADIENT);
 
-        standardImm.setChangeStatusListener(new StatusListener() {
-            @Override
-            public void changeStarted() {
-
-            }
-
-            @Override
-            public void changeSuccessed() {
-
-            }
-
-            @Override
-            public void changeFailed() {
-
-            }
-
-            @Override
-            public void updatingBrightnessValue(int currentBrightness) {
-                sb_standard_imm.setProgress(currentBrightness);
-            }
-
-            @Override
-            public void addBrightnessSuccessed() {
-
-            }
-
-            @Override
-            public void addBrightnessFailed() {
-
-            }
-
-            @Override
-            public void minusBrightnessSuccessed() {
-
-            }
-
-            @Override
-            public void minusBrightnessFailed() {
-
-            }
-        });
-
-
-        standardGra.setChangeStatusListener(new StatusListener() {
-            @Override
-            public void changeStarted() {
-
-            }
-
-            @Override
-            public void changeSuccessed() {
-
-            }
-
-            @Override
-            public void changeFailed() {
-
-            }
-
-            @Override
-            public void updatingBrightnessValue(int currentBrightness) {
-                sb_standard_gra.setProgress(currentBrightness);
-            }
-
-            @Override
-            public void addBrightnessSuccessed() {
-
-            }
-
-            @Override
-            public void addBrightnessFailed() {
-
-            }
-
-            @Override
-            public void minusBrightnessSuccessed() {
-
-            }
-
-            @Override
-            public void minusBrightnessFailed() {
-
-            }
-        });
-    }
 
     private void initView() {
 
         sb_standard_imm = findViewById(R.id.sb_standard_imm);
         sb_standard_gra  = findViewById(R.id.sb_standard_gra);
 
+        sb_standard_imm.setOnSeekBarChangeListener(this);
+        sb_standard_gra.setOnSeekBarChangeListener(this);
+
+
         btn1 = findViewById(R.id.btn1);
         btn2 = findViewById(R.id.btn2);
         btn1.setOnClickListener(this);
         btn2.setOnClickListener(this);
 
+    }
+
+    private void initBrightness() {
+        standardImm = BrightnessFactory.createBrightnessImpl(getApplication(), BrightnessPlatform.PLATFORM_STANDARD, IBrightnessChange.Strategy.IMMEDIATELY);
+        standardGra = BrightnessFactory.createBrightnessImpl(getApplication(), BrightnessPlatform.PLATFORM_STANDARD, IBrightnessChange.Strategy.GRADIENT);
+
+        standardImm.setChangeStatusListener(new CustomChangeListener() {
+
+            @Override
+            public void updatingBrightnessValue(int currentBrightness) {
+                sb_standard_imm.setProgress(currentBrightness);
+            }
+
+        });
+
+
+        standardGra.setChangeStatusListener(new CustomChangeListener() {
+
+
+            @Override
+            public void updatingBrightnessValue(int currentBrightness) {
+                sb_standard_gra.setProgress(currentBrightness);
+            }
+
+
+        });
     }
 
 
@@ -151,5 +93,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 standardGra.setBrightness(new Random().nextInt(100));
             }break;
         }
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        if(seekBar == sb_standard_imm){
+            standardImm.forceChangeBrightness(progress);
+        }else if(seekBar == sb_standard_gra){
+            standardGra.forceChangeBrightness(progress);
+        }
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+
     }
 }

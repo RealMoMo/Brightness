584488373f06c61ae2f4506313a6d02cf5714275
炜烨 <czb166@qq.com>
2018-09-20 16:11:00 +0800

    update demo
    add dynamic permissions function

--- app/src/main/java/com/hht/brightness/lib/test/MainActivity.java
+++ app/src/main/java/com/hht/brightness/lib/test/MainActivity.java
@@ -1,5 +1,12 @@
 package com.hht.brightness.lib.test;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,9 +18,14 @@ import com.hht.brightness.i.CustomChangeListener;
 import com.hht.brightness.impl.BaseBrightnessImpl;
 import com.hht.brightness.impl.BrightnessFactory;
 import com.hht.brightness.strategy.change.IBrightnessChange;
+import com.hht.brightness.utils.DefaultLogger;
+
 
 import java.util.Random;
 
+/**
+ * Test Brightness Lib Demo
+ */
 public class MainActivity extends AppCompatActivity implements View.OnClickListener, SeekBar.OnSeekBarChangeListener {
 
     private SeekBar sb_standard_imm;
@@ -24,7 +36,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private BaseBrightnessImpl standardImm;
     private BaseBrightnessImpl standardGra;
 
+    private boolean hasWriteSettingPermission = false;
 
+    private static final int REQUEST_CODE = 0x100;
 
 
     @Override
@@ -32,6 +46,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //ActivityCompat兼容库 不需特意判断版本号再权限检测
+        if(ActivityCompat.checkSelfPermission(getApplication(), Manifest.permission.WRITE_SETTINGS) == PackageManager.PERMISSION_GRANTED){
+            hasWriteSettingPermission = true;
+        }else{
+            hasWriteSettingPermission = false;
+        }
+        DefaultLogger.debug(null,"hasWriteSettingPermission:"+hasWriteSettingPermission);
 
         initView();
         initBrightness();
@@ -85,12 +106,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
+        if(!hasWriteSettingPermission){
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.WRITE_SETTINGS},REQUEST_CODE);
+        }
         switch (v.getId()){
             case R.id.btn1:{
-                standardImm.setBrightness(new Random().nextInt(100));
+                standardImm.setBrightness(new Random().nextInt(255));
             }break;
             case R.id.btn2:{
-                standardGra.setBrightness(new Random().nextInt(100));
+                standardGra.setBrightness(new Random().nextInt(255));
+            }break;
+            default:{
+                //do nothing
             }break;
         }
     }
@@ -111,6 +138,30 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onStopTrackingTouch(SeekBar seekBar) {
+
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        if (requestCode == REQUEST_CODE){
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+               hasWriteSettingPermission = true;
+            }else{
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.WRITE_SETTINGS)) {
+                    new AlertDialog.Builder(this)
+                            .setMessage("Need writing setting permissions")
+                            .setPositiveButton("OK", (dialog1, which) ->
+                                    ActivityCompat.requestPermissions(MainActivity.this,
+                                            new String[]{Manifest.permission.WRITE_SETTINGS},
+                                            REQUEST_CODE))
+                            .setNegativeButton("Cancel", null)
+                            .create()
+                            .show();
+                }
+            }
+        }
 
     }
 }
